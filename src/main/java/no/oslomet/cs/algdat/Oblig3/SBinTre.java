a382e8b9804adf2ea616f16da25a49489c60ba8d
IbbiSecka <s355423@oslomet.no>
2022-10-28 17:13:54 +0200

    Oppgave 3. Koden for å finne første postorden

--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -148,7 +148,18 @@ public class SBinTre<T> {
         throw new UnsupportedOperationException("Ikke kodet ennå!");
     }
 
-    private static <T> Node<T> førstePostorden(Node<T> p) {throw new UnsupportedOperationException("Ikke kodet ennå!");
+    private static <T> Node<T> førstePostorden(Node<T> p) {
+        while (true){
+        if (p.venstre != null){
+            p = p.venstre;
+        }
+        else if (p.høyre != null){
+            p = p.høyre;
+        }
+        else {
+            return p;
+        }
+       }
     }
 
     private static <T> Node<T> nestePostorden(Node<T> p) {
