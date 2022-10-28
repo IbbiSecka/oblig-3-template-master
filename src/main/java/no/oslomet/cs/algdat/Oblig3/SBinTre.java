2971942bd0cbd72ae3405216ebba64563d2796be
IbbiSecka <s355423@oslomet.no>
2022-10-28 17:16:45 +0200

    Oppgave 4. hjelpemetoden postorden.

--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -180,7 +180,16 @@ public class SBinTre<T> {
     }
 
     public void postorden(Oppgave<? super T> oppgave) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if (rot == null){
+            return;
+        }else {
+            Node<T> p = førstePostorden(rot);
+            while (p != null) {
+                oppgave.utførOppgave(p.verdi);
+
+                p = nestePostorden(p);
+            }
+        }
     }
 
     public void postordenRecursive(Oppgave<? super T> oppgave) {
